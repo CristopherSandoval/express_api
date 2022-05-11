0cc0da459229a2d7b9d5498f84caf3842d5c2c0a
CristopherSandoval <christophersandoval1501@gmail.com>
2022-05-11 14:35:07 -0500

    PUT crea un endpoint que se encarga de actualizar un explorer

--- app.js
+++ app.js
@@ -31,7 +31,13 @@ app.post("/v1/explorers", (req, res) => {
     res.status(201).json({ message: "Created" });
   });
 
-
+app.put("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers PUT request ${new Date()}`);
+  console.log(`Update explorer with id ${req.params.id}`);
+  const requestBody = req.body;
+  console.log(requestBody);
+  res.status(200).json({ message: "Updated" });
+});
 
 
 
